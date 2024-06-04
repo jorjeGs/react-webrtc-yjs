bdbcb01cc0634bb245c5933f80a4a6e9b1136e19
jorjeGs <jgs_23072000@outlook.com>
2024-06-03 22:53:36 -0700

    added p2p functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useRef } from 'react'
 import Editor from '@monaco-editor/react'
-import './App.css'
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import { MonacoBinding } from 'y-monaco'
 
 function App() {
-
+  
+  const editorRef = useRef(null)
+  
   const handleEditorDidMount = (editor, monaco) => {
-    editor.focus()
+    editorRef.current = editor
+    //initialize yjs
+    const doc = new Y.Doc() //collection of shared data
+    console.log('doc', doc)
+    //connect to peers (or start connection) with webRT
+    const provider = new WebrtcProvider('monaco-yjs-demo-room', doc) // room1, room2
+    const type = doc.getText('monaco') // doc { "monaco": "what the IDE is showing"}
+    //bind yjs to monaco
+    const monacoBinding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
+    console.log('monacoBinding', monacoBinding)
+    console.log('provider', provider)
+
   }
 
   return (
